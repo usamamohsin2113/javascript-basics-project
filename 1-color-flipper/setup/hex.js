0a14e62ab8bea3f426f988df1aa1ec101897bd8e
usama.mohsin.3975 <usama.mohsin.3975@slashnext.com>
2021-02-06 11:09:16 +0500

    color flipper

--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        let randomNumber = Math.floor(Math.random() * hex.length);
+        hexColor += hex[randomNumber];
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
