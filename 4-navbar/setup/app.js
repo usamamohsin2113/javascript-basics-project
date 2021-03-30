38e06253aa2ac5bff3fd1918211b7bca8939638e
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-03-30 18:14:11 +0500

    navbar

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    console.log("clicked");
+    links.classList.toggle("show-links");
+});
