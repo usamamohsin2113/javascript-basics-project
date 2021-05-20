8a33fa465fe9ff4140aa8b73f2b81104ecc68a63
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-05-20 10:01:23 +0500

    fixed nav

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -6,9 +6,47 @@
 // ********** set date ************
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
-// ********** close links ************
 
+// ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+    //linksContainer.classList.toggle('show-links');
+
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+
+    if (containerHeight === 0) {
+        const linksHeight = links.getBoundingClientRect().height;
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+    else {
+        linksContainer.style.height = 0;
+    }
+})
 // ********** fixed navbar ************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+const navHeight = navbar.getBoundingClientRect().height;
+
+window.addEventListener('scroll', function () {
+    const scrollHeight = window.pageYOffset;
+
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    }
+    else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if (scrollHeight>500) {
+        topLink.classList.add("show-link");
+    }
+    else{
+        topLink.classList.remove("show-link");
+    }
+})
 
 // ********** smooth scroll ************
 // select links
