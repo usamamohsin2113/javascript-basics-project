16295e691b87261096e021324265c9128c6f137a
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-05-21 09:42:27 +0500

    scroll project completed

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -40,13 +40,42 @@ window.addEventListener('scroll', function () {
         navbar.classList.remove("fixed-nav");
     }
 
-    if (scrollHeight>500) {
+    if (scrollHeight > 500) {
         topLink.classList.add("show-link");
     }
-    else{
+    else {
         topLink.classList.remove("show-link");
     }
 })
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+
+        let position = element.offsetTop - navHeight;
+
+        if (!fixedNav) {
+            position -= navHeight;
+        }
+
+        if (navHeight > 82) {
+            position += containerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position
+        });
+        linksContainer.style.height = 0;
+    });
+});
