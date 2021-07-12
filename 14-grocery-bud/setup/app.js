ae97b559e4d3c9638551244e7cd0ab818d59514a
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-07-12 11:11:12 +0500

    Grocerry bug underway

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,11 +1,47 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+    e.preventDefault();
 
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if (value && !editFlag) {
+
+    }
+    else if (value && editFlag) {
+
+    }
+    else {
+        displayAlert('please enter value', 'danger');
+    }
+}
+
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    setTimeout(() => {
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    }, 1000);
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
